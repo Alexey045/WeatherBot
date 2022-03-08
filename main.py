33bfe09eb8faf39aa8a0668922076b02fde727e8
Alexey <krivonosov2004@mail.ru>
2022-03-09 00:19:43 +0500

    new test version. Edit message style.

--- main.py
+++ main.py
@@ -52,29 +52,37 @@ async def weather_request(message: types.Message):
                             f"?lat={city_req[0]['lat']}&lon={city_req[0]['lon']}"
                             f"&units=metric&appid={KEY}").text)
                     await message.reply(
-                        f'{emoji.emojize(f":cityscape:")}'
-                        f'City: {bold(city_req[0]["name"])}.\n\n' + "\n".join(
-                            str(emoji.emojize(f":keycap_{i + 1}:") + bold(datetime.datetime.fromtimestamp(
-                                req["daily"][i]["dt"]).strftime(
-                                '%B %d, %A'))) + f":\n    {emoji.emojize(':sun:')}"
-                                                 f"Morning: "
-                                                 f"{round(float(req['daily'][i]['temp']['morn']))}"
-                                                 f"\u00A0°C,"
-                                                 f" Day: "
-                                                 f"{round(float(req['daily'][i]['temp']['day']))}"
-                                                 f"\u00A0°C,"
-                                                 f"\n    {emoji.emojize(':new_moon_face:')}"
-                                                 f"Eve: "
-                                                 f"{round(float(req['daily'][i]['temp']['eve']))}"
-                                                 f"\u00A0°C,"
-                                                 f" Night: "
-                                                 f"{round(float(req['daily'][i]['temp']['night']))}"
-                                                 f"\u00A0°C.\n"
+                        f'{emoji.emojize(f":cityscape:")} '
+                        f'City: {bold(city_req[0]["name"])}\n\n' + "\n".join(
+                            str(emoji.emojize(f":keycap_{i + 1}:") + " " +
+                                bold(datetime.datetime.fromtimestamp(
+                                    req["daily"][i]["dt"]).strftime(
+                                    '%B %d, %A'))) + f":\n    {emoji.emojize(':sun:')} "
+                                                     f"Morning: "
+                                                     f"{round(float(req['daily'][i]['temp']['morn']))}"
+                                                     f"\u00A0°C,"
+                                                     f" Day: "
+                                                     f"{round(float(req['daily'][i]['temp']['day']))}"
+                                                     f"\u00A0°C."
+                                                     f"\n    {emoji.emojize(':new_moon_face:')} "
+                                                     f"Eve: "
+                                                     f"{round(float(req['daily'][i]['temp']['eve']))}"
+                                                     f"\u00A0°C,"
+                                                     f" Night: "
+                                                     f"{round(float(req['daily'][i]['temp']['night']))}"
+                                                     f"\u00A0°C.\n"
                             for i in range(len(req["daily"]) - 1)), parse_mode=ParseMode.MARKDOWN)
                 else:
                     await message.reply("Please, write name of the city.")
     else:
         await message.reply("Please, write name of the city.")
+
+
+@dp.message_handler(commands=['settings'], content_types=[ContentType.TEXT])
+async def settings(message: types.Message):
+    await message.answer('Your settings:\n'
+                         '\t daily:\n'
+                         '\t current:')
 
 
 @dp.message_handler(commands=['current'], content_types=[ContentType.TEXT])
@@ -86,7 +94,7 @@ async def weather_request(message: types.Message):
         if city.isnumeric():
             await message.reply("Please, write name of the city.")
         else:
-            if len(city) > 150:
+            if len(city) > 150:  # WTF IS THAT???
                 await message.reply("Error: long name of the city.")  # ToDo
                 return
             else:
@@ -112,31 +120,28 @@ async def weather_request(message: types.Message):
                             # await message.reply("Invalid API key.") # ToDo
                         case _:
                             await message.reply(
-                                f'{emoji.emojize(f":cityscape:")}'
-                                f'City: {req["name"]},\n'
-                                f'{emoji.emojize(f":thermometer:")}'
-                                f'Temperature: {round(float(req["main"]["temp"]))}\u00A0°C,\n'
-                                f'{emoji.emojize(f":dashing_away:")}'
-                                f'Wind speed: {req["wind"]["speed"]}\u00A0m/s,\n'
-                                f'{emoji.emojize(f":droplet:")}'
-                                f'Humidity: {req["main"]["humidity"]}%.')
+                                f'{emoji.emojize(f":cityscape:")} '
+                                f'City: {req["name"]}d\n'
+                                f'{emoji.emojize(f":thermometer:")} '
+                                f'Temperature: {round(float(req["main"]["temp"]))}\u00A0°C\n'
+                                f'{emoji.emojize(f":dashing_away:")} '
+                                f'Wind speed: {req["wind"]["speed"]}\u00A0m/s\n'
+                                f'{emoji.emojize(f":droplet:")} '
+                                f'Humidity: {req["main"]["humidity"]}%')
                 else:
                     await message.reply("Please, write name of the city.")
     else:
         await message.reply("Please, write name of the city.")
 
 
-"""
-@dp.message_handler(commands=['settings'], content_types=[ContentType.TEXT])
-async def settings(message: types.Message):
-    await message.answer('Your settings:\n'
-                         '\t daily:\n'
-                         '\t current:')
-"""
-
-
 @dp.message_handler(commands="help")
 async def cmd_help(message: types.Message):
+    await message.answer("/current [city name] - current weather\n"
+                         "/daily [city name] - daily weather forecast")
+
+
+@dp.message_handler(commands="about")
+async def cmd_about(message: types.Message):
     await message.answer("Weather Bot is @PythonEater personal bot, which will send daily and "
                          "week forecast. Work in progress.")
 
@@ -163,3 +168,8 @@ async def cmd_geotag_message(message: types.Message):
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+    """
+    loop = asyncio.get_event_loop()
+    while True:
+        loop.run_until_complete(schedule.run_pending())
+    """
